b825351227dade4d4599d2ac814f8376b1d3defd
apprehen <3514392356@qq.com>
2023-04-15 18:01:51 +0800

    测试webhook

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ let res = 'C:\\Users\\35143\\AppData\\Local\\Temp\\tmpjmagu5e9.wav'
 // 提取temp文件夹中的文件名
 let fileName = res.replace('C:\\Users\\35143\\AppData\\Local\\Temp\\','').split('.')[0]
 console.log(fileName, typeof fileName)
+
+// explosion!!
