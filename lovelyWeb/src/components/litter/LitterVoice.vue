3680628b06b74b095843bd2f3aa7931ab449dd4f
apprehen <3514392356@qq.com>
2023-04-20 11:28:39 +0800

    解决语音播放问题和时间报错飘红问题

--- lovelyWeb/src/components/litter/LitterVoice.vue
+++ lovelyWeb/src/components/litter/LitterVoice.vue
@@ -1,85 +1,90 @@
 <template>
   <div class="littervoice">
     <div class="chat">
-      <div class="chat-text" @clike="btnPlay">
-      <!-- <audio :src="props.voiceUrl" controls ref="audio" id="audio"></audio> -->
-      <audio :src="voiceUrl" controls ref="musicAudio" id="audio"  style="display: none" ></audio>
-      <div class="yuyin-title">
-        <img src="@/assets/img/yu.png" alt="" class="yuyin-img">
-        <span class="yuyin-time">{{ time }}"</span>
+      <div class="chat-text" @click="btnPlay">
+        <!-- <audio :src="props.voiceUrl" controls ref="audio" id="audio"></audio> -->
+        <audio :src="voiceUrl" controls ref="musicAudio" id="audio" style="display: none"></audio>
+        <div class="yuyin-title">
+          <img src="@/assets/img/yu.png" alt="" class="yuyin-img">
+          <span class="yuyin-time">{{ prTime }}"</span>
+        </div>
       </div>
+      <div class="change">
+        <span>转文字</span>
       </div>
-    <div class="change">
-      <span>转文字</span>
-    </div>
     </div>
     <div class="info-time">
-        <img :src="person?.avatar" alt=""/>
-        <span>{{ person?.name }}</span>
-        <span>{{ person?.time }}</span>
-      </div>
+      <img :src="person?.avatar" alt="" />
+      <span>{{ person?.name }}</span>
+      <span>{{ person?.time }}</span>
+    </div>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { reactive, ref, nextTick, onMounted } from 'vue'
 import voiceUrl from '@/assets/video/zaoshanghao.mp3'
+import { cond } from 'lodash';
 const props = defineProps({
   voiceUrl: String,
   person: Object,
   voiceTime: Number,
 })
 //语音时间 
-let time =ref(parseInt(props.voiceTime))
-
+// let time =ref(parseInt(props.voiceTime))
+let time: any = ref(props.voiceTime)
+let prTime: number = parseInt(time.value)
 //播放语音
-let musicAudio = ref()
-console.log(musicAudio)
-function play() {
-    musicAudio.value.play()
+const btnPlay = () => {
+  console.log('播放语音')
+  const audio = document.getElementById('audio') as HTMLAudioElement
+  audio.play()
+  console.log(audio.play)
 }
-function btnPlay() {
-    play()
-}
-
 // console.log(props.voiceTime)
 </script>
 <style scoped lang='less'>
-.littervoice{
+.littervoice {
   width: 65%;
 }
-.chat{
+
+.chat {
   width: 80%;
   display: flex;
   align-items: center;
 
   .chat-text {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  max-width: 90%;
-  padding-top: 15px;
-  padding-bottom: 7px;
-  padding-left: 20px;
-  padding-right: 20px;
-  border-radius: 20px 20px 20px 5px;
-  background-color: #ffffff;
-  color: #ffffff;
-  cursor: pointer;
-    .yuyin-img{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    max-width: 90%;
+    padding-top: 15px;
+    padding-bottom: 7px;
+    padding-left: 20px;
+    padding-right: 20px;
+    border-radius: 20px 20px 20px 5px;
+    background-color: #ffffff;
+    color: #ffffff;
+    cursor: pointer;
+
+    .yuyin-img {
       width: 80px;
       height: 40px;
       margin-left: -10px;
       margin-bottom: 2px;
       color: #000000;
     }
+
     &:hover {
-    background-color: rgb(156, 155, 155);
+      background-color: rgb(156, 155, 155);
     }
-    .yuyin-title{
+
+    .yuyin-title {
       display: flex;
+      width: 100%;
       align-items: center;
-      .yuyin-time{
+
+      .yuyin-time {
         font-size: 18px;
         color: #000000;
         margin-bottom: 5px;
@@ -87,7 +92,8 @@ function btnPlay() {
     }
 
   }
-  .change{
+
+  .change {
     background-color: rgb(161, 153, 153);
     height: 25px;
     border-radius: 15px;
