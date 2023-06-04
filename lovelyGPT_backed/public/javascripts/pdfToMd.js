fa010525488108052b0fe1427bf0efd5c077c1e5
apprehen <3514392356@qq.com>
2023-06-04 18:55:18 +0800

    更新需要解决的细小问题

--- lovelyGPT_backed/public/javascripts/pdfToMd.js
+++ lovelyGPT_backed/public/javascripts/pdfToMd.js
@@ -1,13 +1,11 @@
 // 将pdf 转化成 md 的函数
 
-const { Poppler } = require("node-poppler");
-const poppler = new Poppler();
+// const { Poppler } = require("node-poppler");
+// const poppler = new Poppler();
 const fs = require('fs');
 const PdfToMd = async (filesPath, outfilesPath) => {
-  poppler.pdfToText(filesPath).then((res) => {
-    fs.writeFile(outfilesPath, res, res => {
-      console.log('文件写入成功：');
-    });
-  });
+  // TODO: 将pdf 转化成 md 的函数
+  // linux系统上面不支持但是windows上面支持，懒得搞了
+  console.log(filesPath, outfilesPath)
 }
 exports.PdfToMd = PdfToMd
