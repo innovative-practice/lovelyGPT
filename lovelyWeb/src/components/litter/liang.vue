7fb48365cbcc183a52afe2112fcf13c1541b32c0
apprehen <3514392356@qq.com>
2023-06-05 16:31:26 +0800

    部署到服务器成功

--- lovelyWeb/src/components/litter/liang.vue
+++ lovelyWeb/src/components/litter/liang.vue
@@ -4,11 +4,19 @@
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 // @ts-nocheck
-import { reactive, ref, onMounted } from 'vue'
-const lovely = '/src/assets/liang/liang.model3.json'
-const liang_warpper = document.getElementById('liang')
+import { reactive, ref, onMounted } from "vue";
+// import lovely from "@/assets/liang/liang.model3.json";
+// import axios from "axios";
+let lovely = "/liang/liang.model3.json";
+// const lovely = "/assets/liang/liang.model3.json";
+// const lovely = "../../../public/liang/liang.model3.json";
+// axios.get("../../../public/liang/liang.model3.json").then((res) => {
+//   // console.log(res)
+//   lovely.value = res.data;
+// });
+const liang_warpper = document.getElementById("liang");
 const liang = async () => {
   const app = new PIXI.Application({
     view: document.getElementById("canvas"),
@@ -16,22 +24,19 @@ const liang = async () => {
     transparent: true,
     resolution: window.devicePixelRatio || 1,
   });
-  app.width = liang_warpper?.clientWidth || 500
-  app.height = liang_warpper?.clientHeight
+  app.width = liang_warpper?.clientWidth || 500;
+  app.height = liang_warpper?.clientHeight;
   const model4 = await PIXI.live2d.Live2DModel.from(lovely);
   model4.x = -50;
   model4.y = -30;
   app.stage.addChild(model4);
   model4.scale.set(0.15);
-}
-onMounted(
-  async () => {
-    await liang()
-  }
-)
-
+};
+onMounted(async () => {
+  await liang();
+});
 </script>
-<style scoped lang='less'>
+<style scoped lang="less">
 .liang {
   width: 100%;
   height: 74vh;
